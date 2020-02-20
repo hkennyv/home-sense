651ec150d59d59e0cc4b615a3ae4ef32b43f914d
khuynh <hkennyv@gmail.com>
2020-02-20 00:12:21 -0800

    add TODOs for BME operation

--- esp32-firmware/include/bme280.h
+++ esp32-firmware/include/bme280.h
@@ -1,7 +1,36 @@
 #ifndef BME280_H
 #define BME280_H
 
+/*
+ * BME280 operation
+ * 
+ * BME280 starts in sleep mode on power up, we want to switch it to normal mode.
+ * 
+ * TODO: set ctrl_meas register @ 0xF4 with temp oversampling config, pressure
+ *  oversampling config, and NORMAL mode
+ * TODO: set config register @ 0xF5 with inactive duration, IIR time constant,
+ *  and SPI enable (disable)
+ * 
+ * TODO: implement burst read for TPH measurements
+ * 
+ * TODO: implement soft reset @ 0xE0
+ * 
+ * 
+ */
+
+/*
+ * bme280_burst_read
+ * 
+ * does a burst read of temp and pressure
+ */
+
 esp_err_t bme280_burst_read(void);
+
+/*
+ * bme280_read_id
+ * 
+ * does a one-time read of the bme280's ID. it should return 0x60
+ */
 esp_err_t bme280_read_id(void);
 
 #endif
